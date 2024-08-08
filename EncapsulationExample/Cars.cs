5e52331ec2de1492380fa4f5f0c9bcb84d5e287c
Ceyda Turkdogan <ceyda.turkdogan@gmail.com>
2024-08-08 11:37:06 +0300

    Add project files.

---
+++ EncapsulationExample/Cars.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationExample
+{
+    public class Cars
+    {
+
+        private string _mark;
+        private string _type;
+        private string _color;
+        private double _doorNumber;
+
+        public double DoorNumber
+        {
+            get
+            {
+                return _doorNumber;
+            }
+
+            set
+            {
+                if ((value != 2) && (value != 4))
+                {
+                    _doorNumber = -1;
+                }
+                else
+                {
+                    _doorNumber = value;
+                }
+            }
+        }
+
+        public Cars(string mark,string type,string color,double doorNumber)
+        {
+            _mark = mark;
+            _type = type;
+            _color = color;
+            DoorNumber = doorNumber;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Araba Markası:{_mark}\nAraba Modeli:{_type}\nAraba Rengi:{_color}\nArabanın Kapı Sayısı:{DoorNumber}");
+        }
+
+    }
+}
---
+++ EncapsulationExample/Program.cs
@@ -0,0 +1,7 @@
+﻿
+
+using EncapsulationExample;
+
+Cars car = new Cars("Renault", "Clio", "Beyaz",5);
+
+car.DisplayInfo();
